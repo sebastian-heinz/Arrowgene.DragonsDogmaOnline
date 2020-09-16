e8b2fa9f254d7d7b496d456224b6e2708bcafa9b
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2020-09-16 22:25:59 +0800

    update

--- Arrowgene.Ddo.Cli/Command/KeyTransformCommand.cs
+++ Arrowgene.Ddo.Cli/Command/KeyTransformCommand.cs
@@ -13,19 +13,21 @@ namespace Arrowgene.Ddo.Cli.Command
 
         public CommandResultType Run(CommandParameter parameter)
         {
-            string seedHex = "A1 10 03 20 3A E2 E4 A9 0E AC 37 3F 28 6F 64 4C".Replace(" ", "");
+            string seedHex = "C4 1A 5A 0F 1A 32 C1 A4 95 FA 5D C7 C5 A9 06 A4".Replace(" ", "");
 
             DdoRandom rng = new DdoRandom();
             rng.SetSeed(Util.FromHexString(seedHex));
             string key = rng.GenerateKey();
+            //    key = "f23e98HafJdSoaj80QBjhh23oajgklSadrhogh2IJnwJEF58";
             byte[] keyBytes = Encoding.UTF8.GetBytes(key);
 
+            Util.DumpBuffer(new StreamBuffer(keyBytes));
             Console.WriteLine($"Key: {key}");
             Console.WriteLine();
-            
+
             int count = 0xCD;
-            
-            
+
+
             // 0332DF66 | 0FBE18 | movsx ebx,byte ptr ds:[eax] | proc - 1
             byte[] proc1_in = new byte[]
             {
@@ -34,16 +36,15 @@ namespace Arrowgene.Ddo.Cli.Command
             byte[] proc1_out = x(proc1_in, 0xB188E5D7);
             // 00B010E7 | 8D6424 04 | lea esp,dword ptr ss:[esp+4] | proc 1 - end
 
-            
-            byte[] output = new byte[0x210 * 3];
-            
+
+            byte[] output = new byte[0x210 * 2];
+
             b(output, 0x02);
             ba(output);
             bb(output);
 
             while (count > 0)
             {
-
                 // 040DD0A3 | 0FB6C8 | movzx ecx,al 
                 uint eax = rng.Next();
                 uint ecx = (byte) eax;
@@ -70,7 +71,7 @@ namespace Arrowgene.Ddo.Cli.Command
             }
 
             // 013EDFF8 | 5D  | pop ebp  
-            
+
             for (int kindex = 0; kindex < keyBytes.Length; kindex++)
             {
                 bb(output);
@@ -80,19 +81,28 @@ namespace Arrowgene.Ddo.Cli.Command
             }
 
             // 007B9EBB | E9 7841C300 | jmp ddo_dump_fix.13EE038 
-            
+
             // 013EBCBF | F3:A5 | rep movsd | decryption - access 7 (copy) (D1 E8 68 9D)
-            
-            
-            
+
+
             // 013EBA34            | 8B043A                | mov eax,dword ptr ds:[edx+edi]  
-           
+            string server_reply =
+                "00000000D1E8689D69FA8F6C56488F22A6515FF75DD958AA3AEEFE3948C1D0C2BA61719C16C2DEB185341FBA7A96F309BB90EB5DD69AC0A46B3426C2DBA45481C7E22B5D4EBB21F6C8AE690B1A8C48579D18566F8F833FF54C1FF4320C8B2A7EDD87521C5EEE7D712E09FFA893EC9FFCA961078032908CE678FE1E87CF38B0C7F3B67072D94D56B76E4081ABA5DD55EEE74F93E021BE0C63718ED31A83B65DEA5145B5B3F2F9090BB80211010A0B8DE745B48C98256277FA7D99984FEDC4BCA898688708BF0D44285C9598B02A97D4E497F4CEF7F291259ADDFF1A73FAB5A7D0BB5E16454AECB6CA82825BA1839F43AC407EA9DA6AD07A901F1FBB598CF560F8775EAC9C";
+            // utilizes servers response
+            byte[] srbytes = Util.FromHexString(server_reply);
+            byte[] server = new byte[0x210 * 3];
+            for (int i = 0; i < srbytes.Length; i++)
+            {
+                server[0x210 + i] = srbytes[i];
+            }
+
+            combine(server, 0x1);
+
+
             Console.WriteLine("output:");
             Util.DumpBuffer(new StreamBuffer(output));
             Console.WriteLine();
 
-            
-            
 
             return CommandResultType.Exit;
         }
@@ -101,6 +111,67 @@ namespace Arrowgene.Ddo.Cli.Command
         {
         }
 
+
+        private void combine(byte[] output, uint val)
+        {
+            // 013EBA00 | 51 | push ecx 
+            output[0] = (byte) (val & 0xFF);
+            output[1] = (byte) (val >> 8 & 0xFF);
+            output[2] = (byte) (val >> 16 & 0xFF);
+            output[3] = (byte) (val >> 24 & 0xFF);
+            for (uint i = 0; i < 0x210; i++)
+            {
+                uint offset_0 = 0;
+                uint offset_1 = 0x210 + (i * 4);
+                uint offset_2 = 0x420 + (i * 4);
+
+                uint s0 = (uint) (output[offset_0] |
+                                  output[offset_0 + 1] << 8 |
+                                  output[offset_0 + 2] << 16 |
+                                  output[offset_0 + 3] << 24
+                    );
+                uint s1 = (uint) (output[offset_1] |
+                                  output[offset_1 + 1] << 8 |
+                                  output[offset_1 + 2] << 16 |
+                                  output[offset_1 + 3] << 24
+                    );
+                uint s2 = (uint) (output[offset_2] |
+                                  output[offset_2 + 1] << 8 |
+                                  output[offset_2 + 2] << 16 |
+                                  output[offset_2 + 3] << 24
+                    );
+                uint r = s1 * s0;
+                uint r1 = r + s2;
+                uint oflow = 0;
+                if (Uint32OverFlow(r, s2))
+                {
+                    // todo calculate overflow value, add CF
+                    //  edx += 1;
+                }
+
+                uint r2 = r1 + oflow;
+                if (Uint32OverFlow(r1, oflow))
+                {
+                    // todo calculate overflow value, add CF
+                    //  edx += 1;
+                }
+
+                output[offset_2] = (byte) (r2 & 0xFF);
+                output[offset_2 + 1] = (byte) (r2 >> 8 & 0xFF);
+                output[offset_2 + 2] = (byte) (r2 >> 16 & 0xFF);
+                output[offset_2 + 3] = (byte) (r2 >> 24 & 0xFF);
+
+
+                Console.WriteLine("dmp:");
+                Util.DumpBuffer(new StreamBuffer(output));
+                Console.WriteLine();
+            }
+        }
+
+        private bool Uint32OverFlow(ulong a, ulong b)
+        {
+            return a + b > uint.MaxValue;
+        }
 
         private byte[] a(byte[] input)
         {
@@ -224,6 +295,7 @@ namespace Arrowgene.Ddo.Cli.Command
                 output[ebp + 2] = (byte) (edx >> 16 & 0xFF);
                 output[ebp + 3] = (byte) (edx >> 24 & 0xFF);
             }
+
             eax = mem_a;
             ecx = mem_b;
             edx = (uint) (output[0] |
@@ -232,13 +304,13 @@ namespace Arrowgene.Ddo.Cli.Command
                           output[3] << 24
                 );
             edx = edx << (byte) ecx;
-            
+
             offset = eax * 4;
             output[offset] = (byte) (edx & 0xFF);
             output[offset + 1] = (byte) (edx >> 8 & 0xFF);
             output[offset + 2] = (byte) (edx >> 16 & 0xFF);
             output[offset + 3] = (byte) (edx >> 24 & 0xFF);
-            
+
             // test eax, eax
             while (eax > 0)
             {
