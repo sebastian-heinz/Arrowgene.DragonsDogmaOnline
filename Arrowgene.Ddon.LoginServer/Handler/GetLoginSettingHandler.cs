d0a596f6ebc10dfc5b2cea368fccadb5b34d2cbb
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2022-01-10 18:23:10 -0500

    Implement L2C_GET_LOGIN_SETTINGS_RES serializer & char creation fix

--- Arrowgene.Ddon.LoginServer/Handler/GetLoginSettingHandler.cs
+++ Arrowgene.Ddon.LoginServer/Handler/GetLoginSettingHandler.cs
@@ -1,7 +1,12 @@
-﻿using Arrowgene.Ddon.LoginServer.Dump;
+using Arrowgene.Buffers;
+using Arrowgene.Ddon.LoginServer.Dump;
 using Arrowgene.Ddon.Server.Logging;
 using Arrowgene.Ddon.Server.Network;
+using Arrowgene.Ddon.Shared.Entity;
+using Arrowgene.Ddon.Shared.Entity.PacketStructure;
+using Arrowgene.Ddon.Shared.Entity.Structure;
 using Arrowgene.Logging;
+using System.Collections.Generic;
 
 namespace Arrowgene.Ddon.LoginServer.Handler
 {
@@ -18,7 +23,49 @@ namespace Arrowgene.Ddon.LoginServer.Handler
 
         public override void Handle(LoginClient client, Packet packet)
         {
-            client.Send(LoginDump.Dump_20);
+            IBuffer buffer = new StreamBuffer();
+            buffer.WriteInt32(0, Endianness.Big); // error
+            buffer.WriteInt32(0, Endianness.Big); // result
+
+            L2CGetLoginSettingsRes entity = new L2CGetLoginSettingsRes
+            {
+                LoginSetting = {
+                    JobLevelMax = 65,
+                    ClanMemberMax = 100,
+                    CharacterNumMax = 4,
+                    EnableVisualEquip = true,
+                    FriendListMax = 200,
+                    URLInfoList = new List<CDataURLInfo>()
+                    {
+                        new CDataURLInfo {Type = 1, URL = "https://members.dd-on.jp/manual_nfb/"},
+                        new CDataURLInfo {Type = 2, URL = "https://members.dd-on.jp/shop/ingame/stone/detail"},
+                        new CDataURLInfo {Type = 3, URL = "https://members.dd-on.jp/shop/ingame/counter?"},
+                        new CDataURLInfo {Type = 4, URL = "https://members.dd-on.jp/shop/ingame/attention?"},
+                        new CDataURLInfo {Type = 5, URL = "https://members.dd-on.jp/shop/ingame/stone/limit"},
+                        new CDataURLInfo {Type = 6, URL = "https://members.dd-on.jp/shop/ingame/counter?"},
+                        new CDataURLInfo {Type = 7, URL = "https://startup-server.dd-on.jp/opening/entry/ddo/cog_callback/capcharge"},
+                        new CDataURLInfo {Type = 8, URL = "https://members.dd-on.jp/sp_ingame/capcharge/"},
+                        new CDataURLInfo {Type = 9, URL = "http://sample09.html"},
+                        new CDataURLInfo {Type = 10, URL = "http://sample10.html"},
+                        new CDataURLInfo {Type = 11, URL = "http://members.dd-on.jp/sp_ingame/campaign/bnr/bnr01.html?"},
+                        new CDataURLInfo {Type = 12, URL = "https://members.dd-on.jp/sp_ingame/support/index.html"},
+                        new CDataURLInfo {Type = 13, URL = "https://members.dd-on.jp/api/photoup/authorize"},
+                        new CDataURLInfo {Type = 14, URL = "http://frontapi.caplink.jp"},
+                        new CDataURLInfo {Type = 15, URL = "https://frontapi.caplink.jp"},
+                        new CDataURLInfo {Type = 16, URL = "https://members.dd-on.jp/sp_ingame/caplink/index.html"},
+                        new CDataURLInfo {Type = 17, URL = "https://members.dd-on.jp/sp_ingame/campaign/bnr/slide.html"},
+                        new CDataURLInfo {Type = 19, URL = "https://members.dd-on.jp/sp_ingame/capcharge/"},
+                        new CDataURLInfo {Type = 20, URL = "https://companion-img.dd-on.jp/"},
+                    },
+                    NoOperationTimeOutTime = 14400,
+                },
+            };
+            EntitySerializer.Get<L2CGetLoginSettingsRes>().Write(buffer, entity);
+
+            Packet response = new Packet(PacketId.L2C_GET_LOGIN_SETTING_RES, buffer.GetAllBytes(), PacketSource.Server);
+            client.Send(response);
+
+            //client.Send(LoginDump.Dump_20);
         }
     }
 }
---
+++ Arrowgene.Ddon.Shared/Entity/Structure/CDataLoginSetting.cs
@@ -0,0 +1,55 @@
+using Arrowgene.Buffers;
+using System.Collections.Generic;
+
+namespace Arrowgene.Ddon.Shared.Entity.Structure
+{
+    public class CDataLoginSetting
+    {
+        public CDataLoginSetting()
+        {
+            JobLevelMax = 0;
+            ClanMemberMax = 0;
+            EnableVisualEquip = false;
+            FriendListMax = 0;
+            URLInfoList = new List<CDataURLInfo>();
+            NoOperationTimeOutTime = 0;
+        }
+
+        public uint JobLevelMax;
+        public uint ClanMemberMax;
+        public byte CharacterNumMax;
+        public bool EnableVisualEquip;
+        public uint FriendListMax;
+        public List<CDataURLInfo> URLInfoList;
+        public uint NoOperationTimeOutTime;
+    }
+
+    public class CDataLoginSettingSerializer : EntitySerializer<CDataLoginSetting>
+    {
+        public override void Write(IBuffer buffer, CDataLoginSetting obj)
+        {
+            WriteUInt32(buffer, obj.JobLevelMax);
+            WriteUInt32(buffer, obj.ClanMemberMax);
+            WriteByte(buffer, obj.CharacterNumMax);
+            WriteBool(buffer, obj.EnableVisualEquip);
+            WriteUInt32(buffer, obj.FriendListMax);
+            WriteEntityList(buffer, obj.URLInfoList);
+            WriteUInt32(buffer, obj.NoOperationTimeOutTime);
+        }
+
+        public override CDataLoginSetting Read(IBuffer buffer)
+        {
+            CDataLoginSetting obj = new CDataLoginSetting();
+            obj.JobLevelMax = ReadUInt32(buffer);
+            obj.ClanMemberMax = ReadUInt32(buffer);
+            obj.CharacterNumMax = ReadByte(buffer);
+            obj.EnableVisualEquip = ReadBool(buffer);
+            obj.FriendListMax = ReadUInt32(buffer);
+            obj.URLInfoList = ReadEntityList<CDataURLInfo>(buffer);
+            obj.NoOperationTimeOutTime = ReadUInt32(buffer);
+            return obj;
+        }
+    }
+}
+
+
