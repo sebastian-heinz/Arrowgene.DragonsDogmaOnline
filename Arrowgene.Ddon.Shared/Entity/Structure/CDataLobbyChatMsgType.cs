100de61a38efe0aed2def2008a3260cf0e06a849
alborrajo <alvarolopezborr@gmail.com>
2022-01-21 22:17:33 +0100

    added missing chat message types

--- Arrowgene.Ddon.Shared/Entity/Structure/CDataLobbyChatMsgType.cs
+++ Arrowgene.Ddon.Shared/Entity/Structure/CDataLobbyChatMsgType.cs
@@ -1,15 +1,21 @@
 namespace Arrowgene.Ddon.Shared.Entity.Structure.CDataLobbyChatMsgType {
-    // Check packet captures to figure this out
+    
     public enum Type : byte
     {
         Say = 0,
         Shout = 1,
-        // Party = ?,
-        // Tell = ?,
-        Clan = 7
-        // Group = ?,
-        // EntryBoard = ?,
-        // Memo = ?
+        Tell = 2,
+        System = 3,
+        Party = 4,
+        ShoutAll = 5,
+        Group = 6,
+        Clan = 7,
+        Entryboard = 8,
+        ManagementGuideC = 9,
+        ManagementGuideN = 10,
+        ManagementAlertC = 11,
+        ManagementAlertN = 12,
+        ClanNotice = 13
     }
 
 }
