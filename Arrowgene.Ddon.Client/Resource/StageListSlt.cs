747564c2748ff7b3848521af96e0e2d1d3f24710
alborrajo <alvarolopezborr@gmail.com>
2022-02-20 21:41:47 +0100

    Adjusted StageListSlt.Info fields data types

--- Arrowgene.Ddon.Client/Resource/StageListSlt.cs
+++ Arrowgene.Ddon.Client/Resource/StageListSlt.cs
@@ -9,9 +9,9 @@ namespace Arrowgene.Ddon.Client.Resource
         {
             public uint StageNo { get; set; }
             public uint Type { get; set; }
-            public uint RecommendLevel { get; set; }
+            public byte RecommendLevel { get; set; }
             public uint MessageId { get; set; }
-            public byte Version { get; set; }
+            public uint Version { get; set; }
         }
 
         public List<Info> AreaInfos { get; }
@@ -33,9 +33,9 @@ namespace Arrowgene.Ddon.Client.Resource
             Info entry = new Info();
             entry.StageNo = ReadUInt32(buffer);
             entry.Type = ReadUInt32(buffer);
-            entry.RecommendLevel = ReadUInt32(buffer);
+            entry.RecommendLevel = buffer.ReadByte();
             entry.MessageId = ReadUInt32(buffer);
-            entry.Version = buffer.ReadByte();
+            entry.Version = ReadUInt32(buffer);
             return entry;
         }
     }
