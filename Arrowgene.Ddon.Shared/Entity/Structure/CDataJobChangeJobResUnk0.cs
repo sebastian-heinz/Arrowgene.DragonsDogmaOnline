1c4c9d3e28534942f54213855a13f2712158a708
alborrajo <alvarolopezborr@gmail.com>
2022-04-03 15:35:40 +0200

    Identified more CData

--- Arrowgene.Ddon.Shared/Entity/Structure/CDataJobChangeJobResUnk0.cs
+++ Arrowgene.Ddon.Shared/Entity/Structure/CDataJobChangeJobResUnk0.cs
@@ -8,25 +8,25 @@ namespace Arrowgene.Ddon.Shared.Entity.Structure
         public CDataJobChangeJobResUnk0()
         {
             Unk0=0;
-            Unk1=new List<CDataJobChangeJobResUnk0Unk1>();
+            Unk1=new List<CDataCharacterItemSlotInfo>();
         }
 
         public byte Unk0 { get; set; }
-        public List<CDataJobChangeJobResUnk0Unk1> Unk1 { get; set; }
+        public List<CDataCharacterItemSlotInfo> Unk1 { get; set; }
 
         public class Serializer : EntitySerializer<CDataJobChangeJobResUnk0>
         {
             public override void Write(IBuffer buffer, CDataJobChangeJobResUnk0 obj)
             {
                 WriteByte(buffer, obj.Unk0);
-                WriteEntityList<CDataJobChangeJobResUnk0Unk1>(buffer, obj.Unk1);
+                WriteEntityList<CDataCharacterItemSlotInfo>(buffer, obj.Unk1);
             }
 
             public override CDataJobChangeJobResUnk0 Read(IBuffer buffer)
             {
                 CDataJobChangeJobResUnk0 obj = new CDataJobChangeJobResUnk0();
                 obj.Unk0 = ReadByte(buffer);
-                obj.Unk1 = ReadEntityList<CDataJobChangeJobResUnk0Unk1>(buffer);
+                obj.Unk1 = ReadEntityList<CDataCharacterItemSlotInfo>(buffer);
                 return obj;
             }
         }
