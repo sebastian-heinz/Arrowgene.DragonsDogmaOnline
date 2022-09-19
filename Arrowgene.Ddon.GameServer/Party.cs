989eaa002c6010a859548761a7e9024723188390
alborrajo <alvarolopezborr@gmail.com>
2022-09-19 19:04:46 +0200

    context bs that seems to do nothing

--- Arrowgene.Ddon.GameServer/Party.cs
+++ Arrowgene.Ddon.GameServer/Party.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using Arrowgene.Ddon.GameServer;
 using Arrowgene.Ddon.Shared.Entity;
+using Arrowgene.Ddon.Shared.Entity.Structure;
 using Arrowgene.Ddon.Shared.Network;
 
 namespace Arrowgene.Ddon.Shared.Model
@@ -13,12 +15,14 @@ namespace Arrowgene.Ddon.Shared.Model
         {
             Id = ++Instances; // Incase 0 is a reserved ID
             Members = new List<GameClient>();
+            Contexts = new Dictionary<ulong, Tuple<CDataContextSetBase, CDataContextSetAdditional>>();
         }
 
         public uint Id { get; set; }
         public List<GameClient> Members { get; set; }
         public GameClient Leader { get; set; }
         public GameClient Host { get; set; }
+        public Dictionary<ulong, Tuple<CDataContextSetBase, CDataContextSetAdditional>> Contexts { get; set; }
 
         public void SendToAll<TResStruct>(TResStruct res) where TResStruct : class, IPacketStructure, new()
         {
