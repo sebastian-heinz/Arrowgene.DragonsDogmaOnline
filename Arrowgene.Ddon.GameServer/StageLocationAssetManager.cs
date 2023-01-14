6617f4a21cd25c0ff022d789eb0dcdaf8a9b8b2b
alborrajo <alvarolopezborr@gmail.com>
2023-01-14 14:20:38 +0100

    Separated nested dictionary into its own class

--- Arrowgene.Ddon.GameServer/StageLocationAssetManager.cs
+++ Arrowgene.Ddon.GameServer/StageLocationAssetManager.cs
@@ -8,7 +8,7 @@ namespace Arrowgene.Ddon.GameServer
 {
     public class StageLocationAssetManager<T, Y> where T : IStageLocationAsset<Y>
     {
-        protected readonly Dictionary<StageId, Dictionary<Y, List<T>>> _assetDictionary;
+        protected readonly MultiKeyMultiValueDictionary<StageId, Y, T> _assetDictionary;
         protected readonly AssetRepository _assetRepository;
         protected readonly List<T> _assetList;
         protected readonly IDatabase _database;
@@ -18,7 +18,7 @@ namespace Arrowgene.Ddon.GameServer
             _assetRepository = assetRepository;
             _database = database;
             _assetList = assetList;
-            _assetDictionary = new Dictionary<StageId, Dictionary<Y, List<T>>>();
+            _assetDictionary = new MultiKeyMultiValueDictionary<StageId, Y, T>();
 
             Load();
             _assetRepository.AssetChanged += AssetRepositoryOnAssetChanged;
@@ -31,20 +31,7 @@ namespace Arrowgene.Ddon.GameServer
 
         public List<T> GetAssets(StageId stageId, Y subGroupId)
         {
-            if (!_assetDictionary.ContainsKey(stageId))
-            {
-                return new List<T>();
-            }
-
-            Dictionary<Y, List<T>> stageAssets = _assetDictionary[stageId];
-            if (!stageAssets.ContainsKey(subGroupId))
-            {
-                return new List<T>();
-            }
-
-            // TODO check time of day
-
-            return new List<T>(stageAssets[subGroupId]);
+            return _assetDictionary.Get(stageId, subGroupId);
         }
 
         public void Load()
@@ -52,29 +39,7 @@ namespace Arrowgene.Ddon.GameServer
             _assetDictionary.Clear();
             foreach (T asset in _assetList)
             {
-                Dictionary<Y, List<T>> stageAssets;
-                if (_assetDictionary.ContainsKey(asset.StageId))
-                {
-                    stageAssets = _assetDictionary[asset.StageId];
-                }
-                else
-                {
-                    stageAssets = new Dictionary<Y, List<T>>();
-                    _assetDictionary.Add(asset.StageId, stageAssets);
-                }
-
-                List<T> subGroupAssets;
-                if (stageAssets.ContainsKey(asset.SubGroupId))
-                {
-                    subGroupAssets = stageAssets[asset.SubGroupId];
-                }
-                else
-                {
-                    subGroupAssets = new List<T>();
-                    stageAssets.Add(asset.SubGroupId, subGroupAssets);
-                }
-
-                subGroupAssets.Add(asset);
+                _assetDictionary.Add(asset.StageId, asset.SubGroupId, asset);
             }
         }
         
