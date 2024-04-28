c22c30a41dfff2666f14f0ff18135692415c6008
alborrajo <alvarolopezborr@gmail.com>
2024-04-28 21:57:39 +0200

    Generalized instance asset managers

--- Arrowgene.Ddon.GameServer/GatheringItems/InstanceItemManager.cs
+++ Arrowgene.Ddon.GameServer/GatheringItems/InstanceItemManager.cs
@@ -1,44 +1,14 @@
-using System.Collections.Generic;
 using System.Linq;
-using Arrowgene.Ddon.Shared.Entity.Structure;
+using System.Collections.Generic;
+using Arrowgene.Ddon.GameServer.GatheringItems;
 using Arrowgene.Ddon.Shared.Model;
 
-namespace Arrowgene.Ddon.GameServer.GatheringItems
+public abstract class InstanceItemManager : InstanceAssetManager<uint, GatheringItem, InstancedGatheringItem>
 {
-    public abstract class InstanceItemManager<T>
+    protected override List<InstancedGatheringItem> InstanceAssets(List<GatheringItem> originals)
     {
-        public InstanceItemManager()
-        {
-            this._gatheringItemsDictionary = new Dictionary<(StageId, T), List<InstancedGatheringItem>>();
-        }
-
-        private readonly Dictionary<(StageId, T), List<InstancedGatheringItem>> _gatheringItemsDictionary;
-
-        public List<InstancedGatheringItem> GetAssets(CDataStageLayoutId stageLayoutId, T subGroupId)
-        {
-            return GetAssets(StageId.FromStageLayoutId(stageLayoutId), subGroupId);
-        }
-
-        public List<InstancedGatheringItem> GetAssets(StageId stageId, T subGroupId)
-        {
-            if(!_gatheringItemsDictionary.ContainsKey((stageId, subGroupId)))
-            {
-                List<GatheringItem> items = FetchItemsFromRepository(stageId, subGroupId);
-                List<InstancedGatheringItem> instancedItems = items
-                    .Select(item => new InstancedGatheringItem(item))
-                    .Where(instancedItem => instancedItem.ItemNum > 0)
-                    .ToList();
-                _gatheringItemsDictionary.Add((stageId, subGroupId), instancedItems);
-                return instancedItems;
-            }
-            return _gatheringItemsDictionary[(stageId, subGroupId)];
-        }
-
-        public void Clear()
-        {
-            _gatheringItemsDictionary.Clear();
-        }
-
-        protected abstract List<GatheringItem> FetchItemsFromRepository(StageId stage, T subGroupId);
+        return originals.Select(item => new InstancedGatheringItem(item))
+            .Where(instancedAsset => instancedAsset.ItemNum > 0)
+            .ToList();
     }
 }
