0c030bf9243d87543f93887af6b2cd29e045f1ce
alborrajo <alvarolopezborr@gmail.com>
2024-05-08 18:38:19 +0200

    Handle discarding multiple items
    Fixes issue #296

--- Arrowgene.Ddon.GameServer/Handler/ItemConsumeStorageItemHandler.cs
+++ Arrowgene.Ddon.GameServer/Handler/ItemConsumeStorageItemHandler.cs
@@ -1,7 +1,8 @@
-using Arrowgene.Ddon.GameServer.Characters;
+using System;
 using Arrowgene.Ddon.Server;
 using Arrowgene.Ddon.Shared.Entity.PacketStructure;
 using Arrowgene.Ddon.Shared.Entity.Structure;
+using Arrowgene.Ddon.Shared.Model;
 using Arrowgene.Ddon.Shared.Network;
 using Arrowgene.Logging;
 
@@ -11,24 +12,37 @@ namespace Arrowgene.Ddon.GameServer.Handler
     {
         private static readonly ServerLogger Logger = LogProvider.Logger<ServerLogger>(typeof(ItemConsumeStorageItemHandler));
         
-        private readonly ItemManager _itemManager;
-
         public ItemConsumeStorageItemHandler(DdonGameServer server) : base(server)
         {
-            _itemManager = server.ItemManager;
         }
 
         public override void Handle(GameClient client, StructurePacket<C2SItemConsumeStorageItemReq> req)
         {
-            client.Send(new S2CItemConsumeStorageItemRes());
-
-            S2CItemUpdateCharacterItemNtc ntc = new S2CItemUpdateCharacterItemNtc();
-            ntc.UpdateType = 4;
-            foreach (CDataStorageItemUIDList consumeItem in req.Structure.ConsumeItemList)
+            S2CItemConsumeStorageItemRes res = new S2CItemConsumeStorageItemRes();
+            try
             {
-                ntc.UpdateItemList.Add(_itemManager.ConsumeItemInSlot(Server, client.Character, consumeItem.StorageType, consumeItem.SlotNo, consumeItem.Num));
+                S2CItemUpdateCharacterItemNtc ntc = new S2CItemUpdateCharacterItemNtc();
+                ntc.UpdateType = 4;
+                foreach (CDataStorageItemUIDList consumeItem in req.Structure.ConsumeItemList)
+                {
+                    CDataItemUpdateResult itemUpdate;
+                    if(consumeItem.SlotNo == 0)
+                    {
+                        itemUpdate = Server.ItemManager.ConsumeItemByUId(Server, client.Character, consumeItem.StorageType, consumeItem.ItemUId, consumeItem.Num);
+                    }
+                    else
+                    {
+                        itemUpdate = Server.ItemManager.ConsumeItemInSlot(Server, client.Character, consumeItem.StorageType, consumeItem.SlotNo, consumeItem.Num);
+                    }
+                    ntc.UpdateItemList.Add(itemUpdate);
+                }
+                client.Send(ntc);
             }
-            client.Send(ntc);
+            catch(Exception _)
+            {
+                res.Error = (uint)ErrorCode.ERROR_CODE_INSTANCE_AREA_GATHERING_ITEM_ERROR;
+            }
+            client.Send(res);
         }
     }
 }
