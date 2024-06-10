66bd7fbefb1b4b4ef01ebc90238f666d32fcda3d
alborrajo <alvarolopezborr@gmail.com>
2024-06-10 23:46:26 +0200

    Sort bazaar listings from lowest to highest price

--- Arrowgene.Ddon.Database/Sql/Core/DdonSqlDbBazaarExhibition.cs
+++ Arrowgene.Ddon.Database/Sql/Core/DdonSqlDbBazaarExhibition.cs
@@ -25,7 +25,8 @@ namespace Arrowgene.Ddon.Database.Sql.Core
                                                                $"WHERE \"item_id\" = @item_id " +
                                                                $"AND \"state\" = {(byte)BazaarExhibitionState.OnSale} " +
                                                                $"AND \"expire\" > DATETIME(\"now\") " +
-                                                               $"AND \"character_id\" <> @excluded_character_id;";
+                                                               $"AND \"character_id\" <> @excluded_character_id " +
+                                                               $"ORDER BY price ASC;";
         
         private static readonly string SqlDeleteBazaarExhibitionByBazaarId = $"DELETE FROM \"{BazaarExhibitionTableName}\" WHERE \"bazaar_id\"=@bazaar_id;";
         private static readonly string SqlDeleteBazaarExhibitionOutdated = $"DELETE FROM \"{BazaarExhibitionTableName}\" WHERE \"state\"={(byte)BazaarExhibitionState.Idle} AND \"expire\" < DATETIME(\"now\");";
