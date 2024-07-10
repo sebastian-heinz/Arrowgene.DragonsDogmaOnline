edfc30246ed76c1f1ed7da712a786a9a8844cc22
Paul Campbell <pacampbell87@gmail.com>
2024-07-10 09:25:43 -0400

    Fix issue with SQL
    Fixed an issue with the SQL statements in the base file.

--- Arrowgene.Ddon.Database/Files/Database/Script/schema_sqlite.sql
+++ Arrowgene.Ddon.Database/Files/Database/Script/schema_sqlite.sql
@@ -538,24 +538,25 @@ CREATE TABLE IF NOT EXISTS "ddon_priority_quests" (
 );
 
 CREATE TABLE IF NOT EXISTS "ddon_system_mail" (
-    "message_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "character_id" INTEGER NOT NULL,
-    "message_state" INTEGER NOT NULL,
-    "sender_name" VARCHAR(256),
-    "message_title" VARCHAR(256),
-    "message_body" VARCHAR(2048),
-    "send_date" INTEGER,
-    CONSTRAINT fk_ddon_system_mail_character_id FOREIGN KEY ("character_id") REFERENCES ddon_character ("character_id") ON DELETE CASCADE
+	"message_id"	INTEGER NOT NULL,
+	"character_id"	INTEGER NOT NULL,
+	"message_state"	INTEGER NOT NULL,
+	"sender_name"	VARCHAR(256) NOT NULL DEFAULT "",
+	"message_title"	VARCHAR(256) NOT NULL DEFAULT "",
+	"message_body"	VARCHAR(2048) NOT NULL DEFAULT "",
+	"send_date"	INTEGER NOT NULL DEFAULT 0,
+	FOREIGN KEY("character_id") REFERENCES "ddon_character"("character_id") ON DELETE CASCADE,
+	PRIMARY KEY("message_id" AUTOINCREMENT)
 );
 
 CREATE TABLE IF NOT EXISTS "ddon_system_mail_attachment" (
-    "message_id" INTEGER NOT NULL,
-    "attachment_id" INTEGER NOT NULL,
-    "attachment_type" INTEGER NOT NULL,
-    "is_received" BOOLEAN NOT NULL,
-    "param0" VARCHAR(256),
-    "param1" INTEGER,
-    "param2" INTEGER,
-    "param3" INTEGER,
-    CONSTRAINT fk_ddon_system_mail_attachment FOREIGN KEY ("message_id") REFERENCES ddon_character ("ddon_system_mail") ON DELETE CASCADE
+	"message_id"	INTEGER NOT NULL,
+	"attachment_id"	INTEGER NOT NULL,
+	"attachment_type"	INTEGER NOT NULL,
+	"is_received"	INTEGER NOT NULL DEFAULT 0,
+	"param0"	VARCHAR(256) NOT NULL DEFAULT "",
+	"param1"	INTEGER NOT NULL DEFAULT 0,
+	"param2"	INTEGER NOT NULL DEFAULT 0,
+	"param3"	INTEGER NOT NULL DEFAULT 0,
+	FOREIGN KEY("message_id") REFERENCES "ddon_system_mail"("message_id") ON DELETE CASCADE
 );
