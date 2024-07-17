cb46e8745535a3303b3df9b3772d1e9257ef8494
Ryan Yappert <ryappert@gmail.com>
2024-07-17 02:02:03 -0700

    Expand ClientItemInfo to handle new properties from a better decoded itemlist.

--- Arrowgene.Ddon.Shared/Csv/ClientItemInfoCsv.cs
+++ Arrowgene.Ddon.Shared/Csv/ClientItemInfoCsv.cs
@@ -4,7 +4,7 @@ namespace Arrowgene.Ddon.Shared.Csv
 {
     public class ClientItemInfoCsv : CsvReaderWriter<ClientItemInfo>
     {
-        protected override int NumExpectedItems => 4;
+        protected override int NumExpectedItems => 12;
 
         protected override ClientItemInfo CreateInstance(string[] properties)
         {
@@ -12,12 +12,33 @@ namespace Arrowgene.Ddon.Shared.Csv
             if (!byte.TryParse(properties[1], out byte category)) return null;
             if (!ushort.TryParse(properties[2], out ushort price)) return null;
             if (!byte.TryParse(properties[3], out byte stackLimit)) return null;
+            if (!byte.TryParse(properties[4], out byte rank)) return null;
+
+            string name = properties[6];
+
+            //Optional properties
+
+            ushort? subcategory = byte.TryParse(properties[5], out byte tmp0) ? (byte?)tmp0 : null;
+            byte? level = byte.TryParse(properties[7], out byte tmp1) ? (byte?)tmp1 : null;
+            byte? jobs = byte.TryParse(properties[8], out byte tmp2) ? (byte?)tmp2 : null;
+            byte? crestSlots = byte.TryParse(properties[9], out byte tmp3) ? (byte?)tmp3 : null;
+            byte? quality = byte.TryParse(properties[10], out byte tmp4) ? (byte?)tmp4 : null;
+            byte? gender = byte.TryParse(properties[11], out byte tmp5) ? (byte?)tmp5 : null;
+
             return new ClientItemInfo
             {
                 ItemId = itemId,
                 Category = category,
                 Price = price,
-                StackLimit = stackLimit
+                StackLimit = stackLimit,
+                Rank = rank,
+                Name = name,
+                SubCategory = subcategory != null ? (ItemSubCategory)subcategory : null,
+                Level = level,
+                JobGroup = jobs != null ? (EquipJobList)jobs : null,
+                CrestSlots = crestSlots,
+                Quality = quality,
+                Gender = gender != null ? (Gender)gender : null,
             };
         }
     }
