61811c073eabc7334932861a1252fff59a31e07e
Ryan Yappert <ryappert@gmail.com>
2024-07-22 23:11:19 -0700

    XP/BO no longer granted for allies who aren't in the same Stage as you.

--- Arrowgene.Ddon.GameServer/Handler/InstanceEnemyKillHandler.cs
+++ Arrowgene.Ddon.GameServer/Handler/InstanceEnemyKillHandler.cs
@@ -130,6 +130,9 @@ namespace Arrowgene.Ddon.GameServer.Handler
                 {
                     memberClient = ((PlayerPartyMember) member).Client;
                     memberCharacter = memberClient.Character;
+
+                    if (memberCharacter.Stage.Id != stageId.Id) continue; //Only nearby allies get XP.
+
                     S2CItemUpdateCharacterItemNtc updateCharacterItemNtc = new S2CItemUpdateCharacterItemNtc();
 
                     if(enemyKilled.BloodOrbs > 0)
@@ -174,6 +177,8 @@ namespace Arrowgene.Ddon.GameServer.Handler
                     Pawn pawn = ((PawnPartyMember) member).Pawn;
                     memberClient = _gameServer.ClientLookup.GetClientByCharacterId(pawn.CharacterId);
                     memberCharacter = pawn;
+
+                    if (memberCharacter.Stage.Id != stageId.Id) continue; //Only nearby allies get XP.
                 }
                 else
                 {
