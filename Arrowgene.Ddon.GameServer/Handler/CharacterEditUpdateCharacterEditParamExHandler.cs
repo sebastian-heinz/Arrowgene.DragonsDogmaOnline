b22c006bbf971dc2b7fe1b871a0343af6a703328
Ryan Yappert <ryappert@gmail.com>
2024-07-30 00:01:29 -0700

    Clean EquipmentTemplates on reincarnation to remove any genderlocked items that can no longer be worn.

--- Arrowgene.Ddon.GameServer/Handler/CharacterEditUpdateCharacterEditParamExHandler.cs
+++ Arrowgene.Ddon.GameServer/Handler/CharacterEditUpdateCharacterEditParamExHandler.cs
@@ -1,8 +1,12 @@
+#nullable enable
 using Arrowgene.Ddon.Server;
 using Arrowgene.Ddon.Server.Network;
 using Arrowgene.Ddon.Shared.Entity.PacketStructure;
+using Arrowgene.Ddon.Shared.Model;
 using Arrowgene.Ddon.Shared.Network;
 using Arrowgene.Logging;
+using System;
+using System.Linq;
 
 namespace Arrowgene.Ddon.GameServer.Handler
 {
@@ -23,6 +27,27 @@ namespace Arrowgene.Ddon.GameServer.Handler
             if(packet.Structure.FirstName.Length > 0) {
                 client.Character.FirstName = packet.Structure.FirstName;
                 Server.Database.UpdateCharacterBaseInfo(client.Character);
+            }
+
+            //Client won't let you reincarnate if you're wearing a gender-locked item, but EquipmentTemplates also have to be cleaned.
+            foreach (JobId job in Enum.GetValues(typeof(JobId)))
+            {
+                foreach (EquipType equipType in Enum.GetValues(typeof(EquipType)))
+                {
+                    foreach (EquipSlot equipSlot in Enumerable.Range(1, 15))
+                    {
+                        Item? item = client.Character.EquipmentTemplate.GetEquipItem(job, equipType, (byte)equipSlot);
+                        if (item is null) continue;
+                        ClientItemInfo itemInfo = Server.ItemManager.LookupInfoByItem(Server, item);
+                        if (itemInfo.Gender == Gender.Any) continue;
+                        if ((client.Character.EditInfo.Sex == 1 && itemInfo.Gender == Gender.Female)
+                            || (client.Character.EditInfo.Sex == 2 && itemInfo.Gender == Gender.Male))
+                        {
+                            client.Character.EquipmentTemplate.SetEquipItem(null, job, equipType, (byte)equipSlot);
+                            Server.Database.DeleteEquipItem(client.Character.CommonId, job, equipType, (byte)equipSlot);
+                        }
+                    }
+                }
             }
 
             client.Send(new S2CCharacterEditUpdateCharacterEditParamExRes());
--- Arrowgene.Ddon.GameServer/Handler/CharacterEditUpdatePawnEditParamExHandler.cs
+++ Arrowgene.Ddon.GameServer/Handler/CharacterEditUpdatePawnEditParamExHandler.cs
@@ -1,11 +1,12 @@
-using System.Linq;
+#nullable enable
 using Arrowgene.Ddon.Server;
 using Arrowgene.Ddon.Server.Network;
 using Arrowgene.Ddon.Shared.Entity.PacketStructure;
-using Arrowgene.Ddon.Shared.Entity.Structure;
 using Arrowgene.Ddon.Shared.Model;
 using Arrowgene.Ddon.Shared.Network;
 using Arrowgene.Logging;
+using System;
+using System.Linq;
 
 namespace Arrowgene.Ddon.GameServer.Handler
 {
@@ -25,6 +26,28 @@ namespace Arrowgene.Ddon.GameServer.Handler
             Server.Database.UpdateEditInfo(pawn);
             pawn.Name = packet.Structure.Name;
             Server.Database.UpdatePawnBaseInfo(pawn);
+
+            //Client won't let you reincarnate if you're wearing a gender-locked item, but EquipmentTemplates also have to be cleaned.
+            foreach (JobId job in Enum.GetValues(typeof(JobId)))
+            {
+                foreach (EquipType equipType in Enum.GetValues(typeof(EquipType)))
+                {
+                    foreach (EquipSlot equipSlot in Enumerable.Range(1, 15))
+                    {
+                        Item? item = pawn.EquipmentTemplate.GetEquipItem(job, equipType, (byte)equipSlot);
+                        if (item is null) continue;
+                        ClientItemInfo itemInfo = Server.ItemManager.LookupInfoByItem(Server, item);
+                        if (itemInfo.Gender == Gender.Any) continue;
+                        if ((pawn.EditInfo.Sex == 1 && itemInfo.Gender == Gender.Female)
+                            || (pawn.EditInfo.Sex == 2 && itemInfo.Gender == Gender.Male))
+                        {
+                            pawn.EquipmentTemplate.SetEquipItem(null, job, equipType, (byte)equipSlot);
+                            Server.Database.DeleteEquipItem(pawn.CommonId, job, equipType, (byte)equipSlot);
+                        }
+                    }
+                }
+            }
+
             client.Send(new S2CCharacterEditUpdatePawnEditParamExRes());
             foreach(Client other in Server.ClientLookup.GetAll()) {
                 other.Send(new S2CCharacterEditUpdateEditParamExNtc() {
