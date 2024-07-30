677e21455626dd23576141cddfe34fb2679d2186
Paul Campbell <pacampbell87@gmail.com>
2024-07-30 18:35:32 -0400

    Add migration strategy

--- Arrowgene.Ddon.Database/DdonDatabaseBuilder.cs
+++ Arrowgene.Ddon.Database/DdonDatabaseBuilder.cs
@@ -14,7 +14,7 @@ namespace Arrowgene.Ddon.Database
         private static readonly ILogger Logger = LogProvider.Logger<Logger>(typeof(DdonDatabaseBuilder));
         private const string DefaultSchemaFile = "Script/schema_sqlite.sql";
 
-        public const uint Version = 6;
+        public const uint Version = 7;
 
         public static IDatabase Build(DatabaseSetting settings)
         {
