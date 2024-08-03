46eff30cc1dad3ff8a1485f445ad9afe5a3dc5a7
Ryan Yappert <ryappert@gmail.com>
2024-08-03 03:04:07 -0700

    Review comments.

--- Arrowgene.Ddon.Database/Files/Database/Script/migration_stamp_sqlite.sql
+++ Arrowgene.Ddon.Database/Files/Database/Script/migration_stamp_sqlite.sql
@@ -4,9 +4,11 @@ CREATE TABLE ddon_stamp_bonus
     "last_stamp_time"      	  DATETIME  	NOT NULL,
     "consecutive_stamp"       INTEGER       NOT NULL,
     "total_stamp"             INTEGER       NOT NULL,
-    CONSTRAINT fk_stamp_bonus_character_id FOREIGN KEY ("character_id") REFERENCES ddon_character ("character_id") ON DELETE CASCADE
+    CONSTRAINT fk_stamp_bonus_character_id FOREIGN KEY ("character_id") REFERENCES "ddon_character" ("character_id") ON DELETE CASCADE
 );
 
 INSERT INTO "ddon_stamp_bonus" SELECT "character_id", "1900-01-01 00:00:00", 0, 0 FROM "ddon_character";
 
-insert or ignore into ddon_wallet_point (character_id, type, value) select character_id, 5, 0 from ddon_character;
+INSERT INTO "ddon_wallet_point"("character_id", "type", "value")
+SELECT "character_id", 5, 0 FROM "ddon_character"
+WHERE NOT EXISTS (SELECT * FROM "ddon_wallet_point" WHERE "ddon_wallet_point"."character_id" = "ddon_character"."character_id" AND "type" = 5);
