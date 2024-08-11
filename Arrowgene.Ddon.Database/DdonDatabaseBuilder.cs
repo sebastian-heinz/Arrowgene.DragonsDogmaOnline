b3f3e765cd1dba5f83f5435093869fa810924d34
Sehkah <sehkah.vhemrada@gmail.com>
2024-08-11 20:35:38 +0200

    Update DB version.

--- Arrowgene.Ddon.Database/DdonDatabaseBuilder.cs
+++ Arrowgene.Ddon.Database/DdonDatabaseBuilder.cs
@@ -14,7 +14,7 @@ namespace Arrowgene.Ddon.Database
         private static readonly ILogger Logger = LogProvider.Logger<Logger>(typeof(DdonDatabaseBuilder));
         private const string DefaultSchemaFile = "Script/schema_sqlite.sql";
 
-        public const uint Version = 9;
+        public const uint Version = 10;
 
         public static IDatabase Build(DatabaseSetting settings)
         {
