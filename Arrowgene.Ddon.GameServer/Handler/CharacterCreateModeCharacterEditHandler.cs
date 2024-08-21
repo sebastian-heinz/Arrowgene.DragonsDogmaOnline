675080f2153524822cc314645e6bfc8bd817c0e3
Paul Campbell <pacampbell87@gmail.com>
2024-08-21 09:45:32 -0400

    Save character edit info
    Save the BBM character model which was changed when entering for the
    first time.

--- Arrowgene.Ddon.GameServer/Handler/CharacterCreateModeCharacterEditHandler.cs
+++ Arrowgene.Ddon.GameServer/Handler/CharacterCreateModeCharacterEditHandler.cs
@@ -1,5 +1,7 @@
 using Arrowgene.Ddon.Server;
+using Arrowgene.Ddon.Server.Network;
 using Arrowgene.Ddon.Shared.Entity.PacketStructure;
+using Arrowgene.Ddon.Shared.Model;
 using Arrowgene.Logging;
 
 namespace Arrowgene.Ddon.GameServer.Handler
@@ -14,6 +16,20 @@ namespace Arrowgene.Ddon.GameServer.Handler
 
         public override S2CCharacterCreateModeCharacterEditParamRes Handle(GameClient client, C2SCharacterCreateModeCharacterEditParamReq request)
         {
+            client.Character.EditInfo = request.EditInfo;
+            Server.Database.UpdateEditInfo(client.Character);
+
+            foreach (Client other in Server.ClientLookup.GetAll())
+            {
+                other.Send(new S2CCharacterEditUpdateEditParamExNtc()
+                {
+                    CharacterId = client.Character.NormalCharacterId,
+                    PawnId = 0,
+                    EditInfo = client.Character.EditInfo,
+                    Name = client.Character.FirstName
+                });
+            }
+
             return new S2CCharacterCreateModeCharacterEditParamRes();
         }
     }
