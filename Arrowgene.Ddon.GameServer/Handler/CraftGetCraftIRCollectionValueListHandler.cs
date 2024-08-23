ccb782e7e6f40bf117e3ae5851630c6e1186a907
Sehkah <sehkah.vhemrada@gmail.com>
2024-08-23 17:40:04 +0200

    Upgrade IR collection value handler to use a decoded list rather than a dump.

--- Arrowgene.Ddon.GameServer/Handler/CraftGetCraftIRCollectionValueListHandler.cs
+++ Arrowgene.Ddon.GameServer/Handler/CraftGetCraftIRCollectionValueListHandler.cs
@@ -1,25 +1,90 @@
-using Arrowgene.Ddon.GameServer.Dump;
+using System.Collections.Generic;
 using Arrowgene.Ddon.Server;
-using Arrowgene.Ddon.Shared.Entity;
 using Arrowgene.Ddon.Shared.Entity.PacketStructure;
-using Arrowgene.Ddon.Shared.Network;
+using Arrowgene.Ddon.Shared.Entity.Structure;
 using Arrowgene.Logging;
 
 namespace Arrowgene.Ddon.GameServer.Handler
 {
-    public class CraftGetCraftIRCollectionValueListHandler : GameStructurePacketHandler<C2SCraftGetCraftIRCollectionValueListReq>
+    public class CraftGetCraftIRCollectionValueListHandler : GameRequestPacketHandler<C2SCraftGetCraftIRCollectionValueListReq, S2CCraftGetCraftIRCollectionValueListRes>
     {
         private static readonly ServerLogger Logger = LogProvider.Logger<ServerLogger>(typeof(CraftGetCraftIRCollectionValueListHandler));
-        
+
+        private static readonly List<CDataCraftSkillRate> SkillRateList = new List<CDataCraftSkillRate>
+        {
+            new CDataCraftSkillRate()
+            {
+                PawnType = 1,
+                SkillType = 1,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 1,
+                SkillType = 2,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 1,
+                SkillType = 3,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 1,
+                SkillType = 4,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 1,
+                SkillType = 5,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 2,
+                SkillType = 1,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 2,
+                SkillType = 2,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 2,
+                SkillType = 3,
+                Rate = 100
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 2,
+                SkillType = 4,
+                Rate = 33
+            },
+            new CDataCraftSkillRate()
+            {
+                PawnType = 2,
+                SkillType = 5,
+                Rate = 33
+            }
+        };
+
         public CraftGetCraftIRCollectionValueListHandler(DdonGameServer server) : base(server)
         {
         }
 
-        // TODO: S2C_CRAFT_GET_CRAFT_IR_COLLECTION_VALUE_LIST_RES
-        public override void Handle(GameClient client, StructurePacket<C2SCraftGetCraftIRCollectionValueListReq> packet)
+        public override S2CCraftGetCraftIRCollectionValueListRes Handle(GameClient client, C2SCraftGetCraftIRCollectionValueListReq request)
         {
-            S2CCraftGetCraftIRCollectionValueListRes res = EntitySerializer.Get<S2CCraftGetCraftIRCollectionValueListRes>().Read(InGameDump.data_Dump_109);
-            client.Send(res);
+            S2CCraftGetCraftIRCollectionValueListRes res = new S2CCraftGetCraftIRCollectionValueListRes();
+
+            res.SkillRateList = SkillRateList;
+
+            return res;
         }
     }
 }
