0fbdb53e4373cb9a677832d79c53b12cefe6423a
Asterit <matt.edwards21@outlook.com>
2024-08-30 21:57:54 +0100

    messy logging

--- Arrowgene.Ddon.GameServer/GatheringItems/InstanceDropItemManager.cs
+++ Arrowgene.Ddon.GameServer/GatheringItems/InstanceDropItemManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using Arrowgene.Ddon.Shared.Model;
+using Arrowgene.Logging;
 
 namespace Arrowgene.Ddon.GameServer.GatheringItems
 {
@@ -15,11 +17,27 @@ namespace Arrowgene.Ddon.GameServer.GatheringItems
         protected override List<GatheringItem> FetchAssetsFromRepository(StageId stage, uint setId)
         {
             List<InstancedEnemy> enemiesInSet =  _client.Party.InstanceEnemyManager.GetAssets(stage, 0);
+
+            //Logger.Debug($"enemiesInSet is not null? {enemiesInSet != null}");
+            Console.WriteLine("======================================= InstanceDropItemManager ==========================================");
+            Console.WriteLine($"enemiesInSet is not null? {enemiesInSet != null}");
+            Console.WriteLine($"setId ({setId}) < enemiesInSet.Count ({enemiesInSet.Count}) ? {setId < enemiesInSet.Count}");
+
             if(enemiesInSet != null && setId < enemiesInSet.Count)
             {
                 Enemy enemy = enemiesInSet[(int) setId];
-                if(enemy.DropsTable != null)
+                Console.WriteLine($"enemy IS {enemy.EnemyId}");
+                Console.WriteLine($"enemy.DropsTable is null? {enemy.DropsTable}");
+
+                if (enemy.DropsTable != null)
                 {
+                    Console.WriteLine($"enemy.DropsTable.Items:");
+
+                    foreach (var item in enemy.DropsTable.Items)
+                    {
+                        Console.WriteLine($"Item: {item}");
+                    }
+
                     return enemy.DropsTable.Items;
                 }
             }
