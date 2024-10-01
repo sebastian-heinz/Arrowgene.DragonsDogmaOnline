25b968c9bc8dd570454f6448158816c5d3192891
Paul Campbell <pacampbell87@gmail.com>
2024-10-01 09:53:08 -0400

    fix: Fix issue with 2.x migration
    Fixed an issue where the MSQ migrations through a constaint faiure exception
    when no characters in the database match the query.

--- Arrowgene.Ddon.Database/Files/Database/Script/migration_msq_2.0.sql
+++ Arrowgene.Ddon.Database/Files/Database/Script/migration_msq_2.0.sql
@@ -1,2 +1,4 @@
 INSERT INTO ddon_quest_progress(character_common_id, quest_type, quest_id, step, variant_quest_id)
-VALUES ((SELECT character_common_id FROM ddon_completed_quests WHERE quest_id = 30), 3, 20010, 0, 0);
+SELECT ddon_completed_quests.character_common_id, 3, 20010, 0, 0
+FROM ddon_completed_quests
+WHERE ddon_completed_quests.quest_id = 30;
