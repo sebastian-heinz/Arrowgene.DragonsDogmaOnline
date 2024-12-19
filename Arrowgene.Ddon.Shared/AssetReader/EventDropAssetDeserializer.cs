24deeffaaf83eaae1b8ff97d83a19c820f61fcd4
Paul Campbell <pacampbell87@gmail.com>
2024-12-19 09:38:20 -0500

    feat: Add enemy constraints for event drop systems
    - Added new EventItemConstraint values
        - LessThan
        - LessThanOrEqual
        - GreaterThan
        - GreaterThanOrEqual
        - InRange
    - Added the ability to define constraints based on enemy level
        - If the constraint is 'InRange' the fields 'min_lv' and 'max_lv'
          are required.
          - "requirements": {"enemy_level" : {"constraint": "InRange", "min_lv": x, "max_lv": y}}
        - If the constraint is any of the other types the field 'lv' is
          required.
          - "requirements": {"enemy_level" : {"constraint": "LessThan|LessThanOrEqual|GreaterThan|GreaterThanOrEqual", "lv": x}}
    - Added the ability to define constraints based on if the enemy is a
      boss or not.
        - "requirements": {"enemy_class": "None|IsBoss|IsNotBoss"}

--- Arrowgene.Ddon.Shared/AssetReader/EventDropAssetDeserializer.cs
+++ Arrowgene.Ddon.Shared/AssetReader/EventDropAssetDeserializer.cs
@@ -79,8 +79,84 @@ namespace Arrowgene.Ddon.Shared.AssetReader
                             else
                             {
                                 Logger.Error($"Failed to parse item constraint {jConstraint.GetString()}");
+                                continue;
                             }
                         }
+                    }
+
+                    eventItem.EmClassConstraint = EventItemConstraint.None;
+                    if (jRequirements.TryGetProperty("enemy_class", out JsonElement jEnemyClass))
+                    {
+                        if (!Enum.TryParse(jEnemyClass.ToString(), out eventItem.EmClassConstraint))
+                        {
+                            Logger.Error("Failed to parse the 'enemy_class' constraint.");
+                            continue;
+                        }
+
+                        if (eventItem.EmClassConstraint != EventItemConstraint.None ||
+                            eventItem.EmClassConstraint != EventItemConstraint.IsBoss ||
+                            eventItem.EmClassConstraint != EventItemConstraint.IsNotBoss)
+                        {
+                            Logger.Error($"The constraint '{eventItem.EmClassConstraint}' is not a valid constraint for 'enemy_class'.");
+                            continue;
+                        }
+                    }
+
+                    eventItem.EmLvConstraint = EventItemConstraint.None;
+                    if (jRequirements.TryGetProperty("enemy_level", out JsonElement jEnemyLevel))
+                    {
+                        if (!Enum.TryParse(jEnemyLevel.GetProperty("constraint").ToString(), out eventItem.EmLvConstraint))
+                        {
+                            Logger.Error("Required element 'constraint' does not exist or is an invalid value.");
+                            continue;
+                        }
+
+                        switch (eventItem.EmLvConstraint)
+                        {
+                            case EventItemConstraint.LessThan:
+                            case EventItemConstraint.LessThanOrEqual:
+                            case EventItemConstraint.GreaterThan:
+                            case EventItemConstraint.GreaterThanOrEqual:
+                            case EventItemConstraint.InRange:
+                                break;
+                            default:
+                                Logger.Error($"The constraint '{eventItem.EmLvConstraint}' is not a valid constraint for 'enemy_level'.");
+                                continue;
+                        }
+
+                        uint Lv = 0;
+                        if (jEnemyLevel.TryGetProperty("min_lv", out JsonElement jEmLv))
+                        {
+                            Lv = jEmLv.GetUInt32();
+                        }
+
+                        uint minLv = 0;
+                        if (jEnemyLevel.TryGetProperty("min_lv", out JsonElement jEmMinLv))
+                        {
+                            minLv = jEmMinLv.GetUInt32();
+                        }
+
+                        uint maxLv = 0;
+                        if (jEnemyLevel.TryGetProperty("max_lv", out JsonElement jEmMaxLv))
+                        {
+                            maxLv = jEmMaxLv.GetUInt32();
+                        }
+
+                        if ((eventItem.EmLvConstraint == EventItemConstraint.LessThan ||
+                             eventItem.EmLvConstraint == EventItemConstraint.LessThanOrEqual ||
+                             eventItem.EmLvConstraint == EventItemConstraint.GreaterThan ||
+                             eventItem.EmLvConstraint == EventItemConstraint.GreaterThanOrEqual) && Lv == 0)
+                        {
+                            Logger.Error($"The constraint '{eventItem.EmLvConstraint}' requires the field 'lv' to be present with a value > 0.");
+                            continue;
+                        }
+                        else if(eventItem.EmLvConstraint == EventItemConstraint.InRange && (minLv == 0 || maxLv == 0))
+                        {
+                            Logger.Error($"The constraint '{eventItem.EmLvConstraint}' requires the field 'min_lv' and 'max_lv' to be present with a value > 0.");
+                            continue;
+                        }
+
+                        eventItem.EmLvConstraintParams = (Lv, minLv, maxLv);
                     }
                 }
 
