dfed136bb0b95d6f18b2de999d19d9e6b2e8d300
Ryan Yappert <ryappert@gmail.com>
2025-01-07 20:57:35 -0800

    The party leader's personal quests can always drive world state.

--- Arrowgene.Ddon.GameServer/Characters/QuestManager.cs
+++ Arrowgene.Ddon.GameServer/Characters/QuestManager.cs
@@ -190,9 +190,9 @@ namespace Arrowgene.Ddon.GameServer.Characters
             var questScheduleIds = new HashSet<uint>();
 
             questScheduleIds.UnionWith(client.Party.QuestState.StageQuests(stageId));
-            if (client.Party.Clients.Count == 1)
+            if (client.Party.Leader is not null)
             {
-                questScheduleIds.UnionWith(client.QuestState.StageQuests(stageId));
+                questScheduleIds.UnionWith(client.Party.Leader.QuestState.StageQuests(stageId));
             }
 
             return questScheduleIds;
