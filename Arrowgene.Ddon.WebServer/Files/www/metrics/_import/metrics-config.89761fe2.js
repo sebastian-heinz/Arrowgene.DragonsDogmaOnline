5c9b566d2dc756911cfa625437f725fb4b77df43
sebastian-heinz <sebastian.heinz.gt@googlemail.com>
2026-03-19 14:47:37 +0800

    up

---
+++ Arrowgene.Ddon.WebServer/Files/www/metrics/_import/metrics-config.89761fe2.js
@@ -0,0 +1,15 @@
+export const servers = ["game", "login"];
+
+export const colorMap = {
+  game: "#00e5ff",
+  login: "#ff6e40"
+};
+
+export const metricFileNames = {
+  timeseries: "timeseries.json",
+  handlers: "handlers.json",
+  durationHistogram: "duration_histogram.json",
+  parseHistogram: "parse_histogram.json",
+  queueDelayHistogram: "queue_delay_histogram.json",
+  receivedHandlerDurationHistogram: "received_handler_duration_histogram.json"
+};
